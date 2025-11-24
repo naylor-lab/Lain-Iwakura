ba277a01dd0e32846fc0719b0220db95f2306385
Lab <naylor-laboratory@tutamail.com>
2025-11-24 22:30:06 +0000

    Comment out example code in app.cpp
    Commented out example code for slash command handling.

--- main/home/app.cpp
+++ main/home/app.cpp
@@ -13,15 +13,21 @@ int main() {
 
      /* The event is fired when someone issues your commands */	
  bot.on_slashcommand([](const dpp::slashcommand_t& event) {
-	        if (event.command.get_command_name() == "ping") {
+	  
+	 /*Exemple:   
+	 if (event.command.get_command_name() == "ping") {
 	            event.reply("Pong!");
 	        }
 	    });
+		*/
 	 
 
 	    bot.on_ready([&bot](const dpp::ready_t& event) {
 	        if (dpp::run_once<struct register_bot_commands>()) {
-	            bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
+				
+	            /*Exemple:
+				bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
+				*/
 	        }
 	    });
 	 
